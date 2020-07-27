adb943a4490414a3c8efb21428802228a261dc97
Rico <47140692+astroreeko@users.noreply.github.com>
2020-07-28 01:51:39 +1000

    Add files via upload

--- memFree.sh
+++ memFree.sh
@@ -4,16 +4,16 @@
 memUsage=$(free -m | grep Mem | awk '{print ($3/$2)*100}')
 
 if [ ${memUsage%.*} -le 17 ]; then
-	echo "mem: | I           "
+	echo "mem: [ I           ]"
 elif [ ${memUsage%.*} -le 35 ]; then
-	echo "mem: | I I         "
+	echo "mem: [ I I         ]"
 elif [ ${memUsage%.*} -le 43 ]; then
-	echo "mem: | I I I       "
+	echo "mem: [ I I I       ]"
 elif [ ${memUsage%.*} -le 60 ]; then
-	echo "mem: | I I I I     "
+	echo "mem: [ I I I I     ]"
 elif [ ${memUsage%.*} -le 77 ]; then
-	echo "mem: | I I I I I   "
+	echo "mem: [ I I I I I   ]"
 else
-	echo "mem: | I I I I I I "
+	echo "mem: [ I I I I I I ]"
 fi
 
